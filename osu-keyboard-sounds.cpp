2b5e842626ece4946d1d77d345a63439fea8e6e6
KerimPotter <48261129+KerimPotter@users.noreply.github.com>
2021-04-05 16:32:02 +0300

    🤯 Changed player to bass.dll and sounds are mp3 now!

--- osu-keyboard-sounds.cpp
+++ osu-keyboard-sounds.cpp
@@ -1,12 +1,19 @@
-// VIRTUAL-KEY-CODES: https://docs.microsoft.com/en-us/windows/win32/inputdev/virtual-key-codes
-
 #include <iostream>
 #include <Windows.h>
-#include <MMSystem.h>
+#include "bass.h"
 
 using namespace std;
 
 HHOOK hHook = NULL;
+
+HSTREAM keyCaps;
+HSTREAM keyConfirm;
+HSTREAM keyDelete;
+HSTREAM keyMovement;
+HSTREAM keyPress1;
+HSTREAM keyPress2;
+HSTREAM keyPress3;
+HSTREAM keyPress4;
 
 LRESULT CALLBACK MyHook(int nCode, WPARAM wParam, LPARAM lParam) {
 
@@ -23,34 +30,34 @@ LRESULT CALLBACK MyHook(int nCode, WPARAM wParam, LPARAM lParam) {
             return 0;
             break;
         case VK_CAPITAL:
-            PlaySound(TEXT("./sounds/key-caps.wav"), NULL, SND_ASYNC);
+            BASS_ChannelPlay(keyCaps, TRUE);
             break;
         case VK_RETURN:
-            PlaySound(TEXT("./sounds/key-confirm.wav"), NULL, SND_ASYNC);
+            BASS_ChannelPlay(keyConfirm, TRUE);
             break;
         case VK_BACK:
-            PlaySound(TEXT("./sounds/key-delete.wav"), NULL, SND_ASYNC);
+            BASS_ChannelPlay(keyDelete, TRUE);
             break;
         case VK_LEFT:
         case VK_UP:
         case VK_RIGHT:
         case VK_DOWN:
-            PlaySound(TEXT("./sounds/key-movement.wav"), NULL, SND_ASYNC);
+            BASS_ChannelPlay(keyMovement, TRUE);
             break;
         default:
             switch (rand() % 4)
             {
             case 0:
-                PlaySound(TEXT("./sounds/key-press-1.wav"), NULL, SND_ASYNC);
+                BASS_ChannelPlay(keyPress1, TRUE);
                 break;
             case 1:
-                PlaySound(TEXT("./sounds/key-press-2.wav"), NULL, SND_ASYNC);
+                BASS_ChannelPlay(keyPress2, TRUE);
                 break;
             case 2:
-                PlaySound(TEXT("./sounds/key-press-3.wav"), NULL, SND_ASYNC);
+                BASS_ChannelPlay(keyPress3, TRUE);
                 break;
             case 3:
-                PlaySound(TEXT("./sounds/key-press-4.wav"), NULL, SND_ASYNC);
+                BASS_ChannelPlay(keyPress4, TRUE);
                 break;
             };
         };
@@ -60,6 +67,16 @@ LRESULT CALLBACK MyHook(int nCode, WPARAM wParam, LPARAM lParam) {
 int main()
 {
     hHook = SetWindowsHookEx(WH_KEYBOARD_LL, MyHook, NULL, NULL);
+    BASS_Init(-1, 44100, 0, 0, NULL);
+
+    keyCaps = BASS_StreamCreateFile(FALSE, "./sounds/keyCaps.mp3", 0, 0, 0);
+    keyConfirm = BASS_StreamCreateFile(FALSE, "./sounds/keyConfirm.mp3", 0, 0, 0);
+    keyDelete = BASS_StreamCreateFile(FALSE, "./sounds/keyDelete.mp3", 0, 0, 0);
+    keyMovement = BASS_StreamCreateFile(FALSE, "./sounds/keyMovement.mp3", 0, 0, 0);
+    keyPress1 = BASS_StreamCreateFile(FALSE, "./sounds/keyPress1.mp3", 0, 0, 0);
+    keyPress2 = BASS_StreamCreateFile(FALSE, "./sounds/keyPress2.mp3", 0, 0, 0);
+    keyPress3 = BASS_StreamCreateFile(FALSE, "./sounds/keyPress3.mp3", 0, 0, 0);
+    keyPress4 = BASS_StreamCreateFile(FALSE, "./sounds/keyPress4.mp3", 0, 0, 0);
 
     MSG msg;
     while (GetMessage(&msg, NULL, 0, 0)) {
